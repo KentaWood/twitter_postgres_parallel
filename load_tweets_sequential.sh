62f955fc466ea0405465ae4ab0bf11009ba85994
KentaWood <you@example.com>
2024-04-08 15:10:27 -0700

    checkpoint2

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -3,18 +3,16 @@
 files=$(find data/*)
 
 echo '================================================================================'
-echo 'load denormalized'
+echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
     	python3 -u load_tweets.py --db postgresql://postgres:pass@localhost:1331 --inputs $file
 done
 
 echo '================================================================================'
-echo 'load pg_normalized'
+echo 'load pg_denormalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
         unzip -p $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1330 -c "COPY tweets_jsonb (data)  FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
